6c2204c5c945a460a94181cf1b6c601b25e5ea3d
SEED-JYK-LT\admin <likerdo@naver.com>
2019-08-28 11:22:18 +0900

    기본 셋팅

---
+++ src/main/java/com/likerdo/lms/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.likerdo.lms.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+    @GetMapping
+    public String hello(){
+        return "HelloWorld";
+    }
+
+}
